9897777be0bf35fae47e34ea66e0633a530f8174
luca2 <luca200284@googlemail.com>
2022-04-26 00:28:51 +0100

    javadoc on Rock class

--- src/main/java/Rock.java
+++ src/main/java/Rock.java
@@ -1,23 +1,42 @@
 import java.util.ArrayList;
 
+/**
+ * Class models the behaviour of the rock weapon
+ * @author Luca Khatri
+ * @
+ */
 public class Rock extends Power {
 
     /**
      * Power constructor
      *
-     * @param xPos       x coordinate
-     * @param yPos       y coordinate
+     * @param xPos x coordinate
+     * @param yPos y coordinate
      */
     public Rock(int xPos, int yPos) {
         super(false, xPos, yPos);
 
     }
 
+    /**
+     * abstract method that tells every power to do their thing
+     * for rock - it does nothing
+     * @param rats used to interact with all rats that stepped on the power.
+     * @param currentTile used for removing power from its Tile.
+     */
     @Override
     void activate(ArrayList<Rat> rats, Tile currentTile) {
 
     }
 
+    /**
+     * Abstract method for certain powers that need to activate after a
+     * certain amount of time.
+     *
+     * for rock - it does nothing
+     * @param rats used for updating the rat arraylist every game tick.
+     * @param currentTile used for calling removeActivePower(this).
+     */
     @Override
     void onTick(ArrayList<Rat> rats, Tile currentTile) {
 
